e073950a7463074854b2eb097cac565ee89f3b82
MEEP of Faith <54301439+MEEPofFaith@users.noreply.github.com>
2020-12-01 13:50:45 -0800

    Add 0.125x speed and 8x speed because why not.

--- scripts/main.js
+++ scripts/main.js
@@ -1,66 +1,38 @@
 var current = 1;
 
 function addSpeed(t, speed){
-var b = new Button(Styles.logict);
-b.label(prov(() => (current == speed ? "[#a9d8ff]" : "[white]") + "x" + speed + "[]"));
-b.clicked(() => {
-Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * speed, 3 * speed));
-current = speed;
-});
-return t.add(b).size(50, 40).color(Pal.lancerLaser).pad(1);
-}
+  var b = new Button(Styles.logict);
+  b.label(prov(() => (current == speed ? "[#a9d8ff]" : "[white]") + "x" + speed + "[]"));
+  b.clicked(() => {
+    Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * speed, 3 * speed));
+    current = speed;
+  });
 
-
-function addSpeedAlt(t, speed, speed2){
-var b = new Button(Styles.logict);
-b.label(prov(() => (current == speed ? "[#a9d8ff]" : (current == speed2 ? "[accent]" : "[white]")) + "x" + (current == speed2 ? speed2 : speed) + "[]"));
-b.clicked(() => {
-if(current == speed){
-Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * speed2, 3 * speed2));
-current = speed2;
-b.setColor(Pal.accent);
-}
-else{
-Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * speed, 3 * speed));
-current = speed;
-b.setColor(Pal.lancerLaser);
-}
-});
-b.update(() => {
-b.setColor(current == speed2 ? Pal.accent : Pal.lancerLaser);
-});
-return t.add(b).size(50, 40).color(Pal.lancerLaser).pad(1);
+  return t.add(b).size(50, 40).color(Pal.lancerLaser).pad(1);
 }
 
 function addTable(table){
-table.table(Styles.black5, cons(t => {
-t.background(Tex.buttonEdge3);
-if(Vars.mobile){
-addSpeedAlt(t, 0.5, 0.25).width(60);
-addSpeed(t, 1).width(45);
-addSpeedAlt(t, 2, 8).width(45);
-addSpeedAlt(t, 4, 16).width(45);
-}
-else{
-addSpeed(t, 0.25).width(65);
-addSpeed(t, 0.5);
-addSpeed(t, 1);
-addSpeedAlt(t, 2, 8);
-addSpeedAlt(t, 4, 16);
-}
-
-}));
-table.fillParent = true;
-table.visibility = () => Vars.ui.hudfrag.shown && !Vars.ui.minimapfrag.shown();
+  table.table(Styles.black5, cons(t => {
+    t.background(Tex.buttonEdge3);
+    addSpeed(t, 0.125).width(80);
+    addSpeed(t, 0.25);
+    addSpeed(t, 0.5);
+    addSpeed(t, 1);
+    addSpeed(t, 2);
+    addSpeed(t, 4);
+    addSpeed(t, 8);
+  }));
+  table.fillParent = true;
+  table.visibility = () => Vars.ui.hudfrag.shown && !Vars.ui.minimapfrag.shown();
 }
 
 
 if(!Vars.headless){
-var ut = new Table();
+  var ut = new Table();
 
-Events.on(ClientLoadEvent, () => {
-ut.bottom().left();
-addTable(ut);
-Vars.ui.hudGroup.addChild(ut);
-});
+  Events.on(ClientLoadEvent, () => {
+    ut.bottom().left();
+    addTable(ut);
+    Vars.ui.hudGroup.addChild(ut);
+  });
 }

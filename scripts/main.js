26f65646131de1eac4a991dfe0e8e5000435866c
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2020-12-01 15:45:18 +0900

    i give up

--- scripts/main.js
+++ scripts/main.js
@@ -13,13 +13,13 @@ return t.add(b).size(50, 40).color(Pal.lancerLaser).pad(1);
 
 function addTable(table){
 table.table(Styles.black5, cons(t => {
-t.background(Tex.buttonEdge4);
+t.background(Tex.buttonEdge3);
 addSpeed(t, 0.25).width(65);
 addSpeed(t, 0.5);
 addSpeed(t, 1);
 addSpeed(t, 2);
 addSpeed(t, 4);
-})).width(Core.scene.find("waves").getWidth()-36);
+}));
 table.fillParent = true;
 table.visibility = () => Vars.ui.hudfrag.shown && !Vars.ui.minimapfrag.shown();
 }
@@ -29,12 +29,8 @@ if(!Vars.headless){
 var ut = new Table();
 
 Events.on(ClientLoadEvent, () => {
-ut.top().left();
+ut.bottom().left();
 addTable(ut);
-  var status = Core.scene.find("status");
-  ut.update(() => {
-    ut.marginTop(status.getHeight());
-  });
 Vars.ui.hudGroup.addChild(ut);
 });
 }

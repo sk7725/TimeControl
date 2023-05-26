bae232b0c06ce5e3431a6f3e4912db44bab4611a
MEEP of Faith <meepmatthew@gmail.com>
2023-05-25 22:59:33 -0700

    Folded button

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,21 @@
 let cols = [Pal.lancerLaser, Pal.accent, Color.valueOf("cc6eaf")]; //Pink from BetaMindy
 let folded = false;
-let speeds = [];
-for(let i = -8; i <= 8; i++){
-    speeds[i + 8] = Math.pow(2, i);
-}
+let curSpeed = 0;
+let longPress = 30;
+let unfoldTimer = 0;
 
-function addTable(table){
-    table.table(Tex.pane, t => {
-        let s = new Slider(-8, 8, 1, false);
-        s.setValue(0);
+let timeSlider = null;
+let foldedButton = null;
+
+function sliderTable(table){
+    table.table(Tex.buttonEdge3, t => {
+        timeSlider = new Slider(-8, 8, 1, false);
+        timeSlider.setValue(0);
+        
         let l = t.button("[accent]x1", () => {
+            curSpeed = Mathf.clamp(curSpeed, -2, 2) - 1;
+            foldedButton.fireClick();
             folded = true;
-            Log.info("bazinga");
         }).grow().width(10.5 * 8).get();
         l.margin(0);
         l.marginLeft(6);
@@ -21,40 +25,87 @@ function addTable(table){
         lStyle.over = Tex.flatDownBase;
         lStyle.down = Tex.whitePane;
         
-        let b = t.button(new TextureRegionDrawable(Icon.refresh), 24, () => s.setValue(0)).padLeft(6).get();
+        let b = t.button(new TextureRegionDrawable(Icon.refresh), 24, () => timeSlider.setValue(0)).padLeft(6).get();
         b.getStyle().imageUpColor = Pal.accent;
-        t.add(s).padLeft(6).minWidth(200);
-        s.moved(v => {
-            let t = Math.pow(2, v);
-            Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * t, 3 * t));
-            Tmp.c1.lerp(cols, (s.getValue() + 8) / 16);
+        t.add(timeSlider).padLeft(6).minWidth(200);
+        timeSlider.moved(v => {
+            curSpeed = v;
+            let speed = Math.pow(2, v);
+            Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * speed, 3 * speed));
             
-            let text = "[#" + Tmp.c1.toString() + "]";
-            if(v >= 0){
-                text += "x" + Math.pow(2, v);
-            }else{
-                text += "x1/" + Math.pow(2, Math.abs(v));
-            }
+            Tmp.c1.lerp(cols, (timeSlider.getValue() + 8) / 16);
             
-            l.setText(text);
+            l.setText(speedText(v));
         });
     });
-    table.visibility = () => {
-        if(!Vars.ui.hudfrag.shown || Vars.ui.minimapfrag.shown()) return false;
-        if(!Vars.mobile) return true;
-        
-        let input = Vars.control.input;
-        return input.lastSchematic == null || input.selectPlans.isEmpty();
-    };
+    table.visibility = () => !folded && visibility();
 }
 
-if(!Vars.headless){
-    var tc = new Table();
+function foldedButtonTable(table){
+    table.table(Tex.buttonEdge3, t => {
+        foldedButton = t.button("[accent]x1", () => {
+            curSpeed++;
+            if(curSpeed > 2) curSpeed = -2;
+            
+            let speed = Math.pow(2, curSpeed);
+            Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * speed, 3 * speed));
+            
+            foldedButton.setText(speedText(curSpeed));
+            timeSlider.setValue(curSpeed);
+        }).grow().width(10.5 * 8).get();
+        foldedButton.margin(0);
+        foldedButton.marginLeft(6);
+        foldedButton.getLabel().setAlignment(Align.left);
+        
+        foldedButton.update(() => {
+            if(foldedButton.isPressed()){
+                unfoldTimer += Core.graphics.getDeltaTime() * 60;
+                if(unfoldTimer > longPress && folded){
+                    folded = false;
+                    unfoldTimer = 0;
+                }
+            }else{
+                unfoldTimer = 0;
+            }
+        });
+    }).height(72);
+    table.visibility = () => folded && visibility();
+}
 
+function speedText(speed){
+    Tmp.c1.lerp(cols, (speed + 8) / 16);
+    let text = "[#" + Tmp.c1.toString() + "]";
+    if(speed >= 0){
+        text += "x" + Math.pow(2, speed);
+    }else{
+        text += "x1/" + Math.pow(2, Math.abs(speed));
+    }
+    return text;
+}
+
+function visibility(){
+    if(!Vars.ui.hudfrag.shown || Vars.ui.minimapfrag.shown()) return false;
+    if(!Vars.mobile) return true;
+    
+    let input = Vars.control.input;
+    return input.lastSchematic == null || input.selectPlans.isEmpty();
+}    
+
+if(!Vars.headless){
     Events.on(ClientLoadEvent, () => {
-        tc.bottom().left();
-        addTable(tc);
-        Vars.ui.hudGroup.addChild(tc);
-        if(Vars.mobile) tc.moveBy(0, Scl.scl(46));
+        let ft = new Table();
+        ft.bottom().left();
+        foldedButtonTable(ft);
+        Vars.ui.hudGroup.addChild(ft);
+        
+        let st = new Table();
+        st.bottom().left();
+        sliderTable(st);
+        Vars.ui.hudGroup.addChild(st);
+        
+        if(Vars.mobile){
+            st.moveBy(0, Scl.scl(46));
+            ft.moveBy(0, Scl.scl(46));
+        }
     });
 }

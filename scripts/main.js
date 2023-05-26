d5d3d4862ed3d09b09f6e55ff5086617ef9fd275
MEEP of Faith <meepmatthew@gmail.com>
2023-05-25 22:00:07 -0700

    The label is now a button

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,42 @@
-//Pink from BetaMindy
-let cols = [Pal.lancerLaser, Pal.accent, Color.valueOf("cc6eaf")];
+let cols = [Pal.lancerLaser, Pal.accent, Color.valueOf("cc6eaf")]; //Pink from BetaMindy
+let folded = false;
+let speeds = [];
+for(let i = -8; i <= 8; i++){
+    speeds[i + 8] = Math.pow(2, i);
+}
 
 function addTable(table){
     table.table(Tex.pane, t => {
         let s = new Slider(-8, 8, 1, false);
         s.setValue(0);
-        let l = t.label(() => {
-            let v = s.getValue();
-            if(v >= 0){
-                return "x" + Math.pow(2, v);
-            }else{
-                return "x1/" + Math.pow(2, Math.abs(v));
-            }
-        }).growX().width(8.5 * 8).color(Pal.accent);
+        let l = t.button("[accent]x1", () => {
+            folded = true;
+            Log.info("bazinga");
+        }).grow().width(10.5 * 8).get();
+        l.margin(0);
+        l.marginLeft(6);
+        l.getLabel().setAlignment(Align.left);
+        let lStyle = l.getStyle();
+        lStyle.up = Tex.pane;
+        lStyle.over = Tex.flatDownBase;
+        lStyle.down = Tex.whitePane;
+        
         let b = t.button(new TextureRegionDrawable(Icon.refresh), 24, () => s.setValue(0)).padLeft(6).get();
         b.getStyle().imageUpColor = Pal.accent;
         t.add(s).padLeft(6).minWidth(200);
         s.moved(v => {
             let t = Math.pow(2, v);
             Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * t, 3 * t));
-            l.color(Tmp.c1.lerp(cols, (s.getValue() + 8) / 16));
+            Tmp.c1.lerp(cols, (s.getValue() + 8) / 16);
+            
+            let text = "[#" + Tmp.c1.toString() + "]";
+            if(v >= 0){
+                text += "x" + Math.pow(2, v);
+            }else{
+                text += "x1/" + Math.pow(2, Math.abs(v));
+            }
+            
+            l.setText(text);
         });
     });
     table.visibility = () => {

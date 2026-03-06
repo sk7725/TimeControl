577e184903ee53026559b58a9f111df0576b7861
Lett <lett@mail.lettsn.org>
2026-03-05 23:56:28 -0500

    Hide slider in multiplayer
    Removing IPs from DOS ban list gets a little annoying fast ngl

--- scripts/main.js
+++ scripts/main.js
@@ -82,6 +82,14 @@ function speedText(speed){
 }
 
 function visibility(){
+    if (Vars.net.active()) {
+        let speed = 1;
+        Time.setDeltaProvider(() => Math.min(Core.graphics.getDeltaTime() * 60 * speed, 3 * speed));
+        foldedButton.setText(speedText(0));
+        timeSlider.setValue(0);
+
+        return false;
+    };
     if(!Vars.ui.hudfrag.shown || Vars.ui.minimapfrag.shown()) return false;
     if(!Vars.mobile) return true;
     
